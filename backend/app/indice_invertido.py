0e7ebef04be66ca02638b818f304e24057e8121d
AaronCoorahua <ruben.coorahua@utec.edu.pe>
2023-10-30 01:58:19 -0500

    Se agrega el algoritmo spimi y se usa el calculo TF-IDF en el indice

--- backend/app/indice_invertido.py
+++ backend/app/indice_invertido.py
@@ -1,13 +1,17 @@
-import pandas as pd
-import nltk
 import math
-import numpy as np
+import pickle
 from collections import defaultdict
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-import pickle
+import pandas as pd
 import os
+
+TERMS_LIMIT = 1000
+stop_words = set(stopwords.words('english'))
+block_num = 0
+
+os.system('del block_*.pkl')
 
 def score_tf(tf):
     return 1 + math.log10(tf) if tf > 0 else 0
@@ -15,60 +19,9 @@ def score_tf(tf):
 def score_idf(N, df_t):
     return math.log10(N/df_t) if df_t > 0 else 0
 
-def nested_defaultdict():
-    return defaultdict(float)
-
-def build_index(docs):
-    inverted_index = defaultdict(list)
-    N = len(docs)
-    df = defaultdict(int)
-
-    for doc_id, doc in enumerate(docs):
-        terms = set(doc.split())
-        for term in terms:
-            inverted_index[term].append(doc_id)
-            df[term] += 1
-
-    idf_values = {term: score_idf(N, df_t) for term, df_t in df.items()}
-    
-    return inverted_index, idf_values
-
-def cosine(query, block_num, num_docs, idf_values):
-    with open(f"block_{block_num}.pkl", "rb") as file:
-        inverted_index = pickle.load(file)
-
-    processed_query = preprocess_text(query)
-    query_terms = processed_query.split()
-    query_tf = defaultdict(int)
-    for term in query_terms:
-        query_tf[term] += 1
-    max_tf = max(query_tf.values())
-
-    tf_idf_query = {}
-    for term in query_terms:
-        if term in idf_values:
-            tf_q = 0.5 + 0.5 * (query_tf[term] / max_tf)
-            tf_idf_query[term] = tf_q * idf_values[term]
-
-    similarities = defaultdict(float)
-    for term, weight in tf_idf_query.items():
-        if term in inverted_index:
-            for doc_id in inverted_index[term]:
-                similarities[doc_id] += weight
-
-    return similarities
-
-def retrieve_top_k(query, k, num_blocks, num_docs, idf_values):
-    overall_similarities = defaultdict(float)
-    for block_num in range(num_blocks):
-        block_similarities = cosine(query, block_num, num_docs, idf_values)
-        for doc_id, score in block_similarities.items():
-            overall_similarities[doc_id] += score
-
-    top_k_indices = sorted(overall_similarities, key=overall_similarities.get, reverse=True)[:k]
-    for idx in top_k_indices:
-        print(f"Doc ID: {idx}, Terms: {docs[idx]}")
-    return df.iloc[top_k_indices]
+def write_block_to_disk(inverted_index, block_num):
+    with open(f"block_{block_num}.pkl", "wb") as file:
+        pickle.dump(inverted_index, file)
 
 def preprocess_text(text):
     text = ' '.join([word for word in word_tokenize(text) if word.isalpha()])
@@ -78,33 +31,86 @@ def preprocess_text(text):
     stemmed_tokens = [stemmer.stem(token) for token in tokens]
     return ' '.join(stemmed_tokens)
 
-def write_block_to_disk(inverted_index, block_num):
-    with open(f"block_{block_num}.pkl", "wb") as file:
-        pickle.dump(inverted_index, file)
+def spimi_invert(docs):
+    global block_num
+    inverted_index = defaultdict(list)
+    unique_terms = set()
 
-BLOCK_SIZE = 4000
-stop_words = set(stopwords.words('english'))
+    for doc_id, doc in enumerate(docs):
+        terms = doc.split()
+        for term in terms:
+            if term not in inverted_index:
+                if len(unique_terms) >= TERMS_LIMIT:
+                    write_block_to_disk(inverted_index, block_num)
+                    block_num += 1
+                    inverted_index.clear()
+                    unique_terms.clear()
+                unique_terms.add(term)
+            inverted_index[term].append(doc_id)
+
+    if inverted_index:
+        write_block_to_disk(inverted_index, block_num)
+
+def calculate_idf_values(num_docs, num_blocks):
+    df = defaultdict(int)
+
+    for block_num in range(num_blocks):
+        with open(f"block_{block_num}.pkl", "rb") as file:
+            block_index = pickle.load(file)
+        for term, postings_list in block_index.items():
+            df[term] += len(set(postings_list))
+
+    idf_values = {term: score_idf(num_docs, df_t) for term, df_t in df.items()}
+    
+    return idf_values
+
+def retrieve_top_k(query, k, num_blocks, num_docs, idf_values):
+    overall_similarities = defaultdict(float)
+    for block_num in range(num_blocks):
+        block_similarities = cosine(query, block_num, num_docs)
+        for doc_id, score in block_similarities.items():
+            overall_similarities[doc_id] += score
+
+    top_k_indices = sorted(overall_similarities, key=overall_similarities.get, reverse=True)[:k]
+    return df.iloc[top_k_indices]
+
+def cosine(query, block_num, num_docs):
+    with open(f"block_{block_num}.pkl", "rb") as file:
+        block_index = pickle.load(file)
+    
+    query_terms = query.split()
+    doc_scores = defaultdict(float)
+
+    for term in query_terms:
+        if term in block_index:
+            df_t = len(block_index[term])
+            idf = score_idf(num_docs, df_t)
+            for doc_id in block_index[term]:
+                tf = block_index[term].count(doc_id)
+                doc_scores[doc_id] += score_tf(tf) * idf
+                
+    # Ponderación adicional para la coincidencia de múltiples términos
+    for doc_id in doc_scores:
+        terms_matched = sum([1 for term in query_terms if term in docs[doc_id]])
+        doc_scores[doc_id] *= terms_matched
+
+    return doc_scores
+
 
 df = pd.read_csv('spotify.csv', on_bad_lines='skip')
 df['combined_text'] = df.apply(lambda row: ' '.join(row.astype(str)), axis=1)
 df['processed_text'] = df['combined_text'].apply(preprocess_text)
 docs = df['processed_text'].tolist()
-num_blocks = (len(docs) + BLOCK_SIZE - 1) // BLOCK_SIZE
 
-inverted_index, idf_values = build_index(docs)
+spimi_invert(docs)
 
-for i in range(num_blocks):
-    start_idx = i * BLOCK_SIZE
-    end_idx = min(start_idx + BLOCK_SIZE, len(docs))
-    block_docs = docs[start_idx:end_idx]
-    inverted_index_block, _ = build_index(block_docs)
-    write_block_to_disk(inverted_index_block, i)
+idf_values = calculate_idf_values(len(df), block_num)
 
-query_original = "Maluma and ozuna"
+query_original = "maluma and ozuna"
 query = preprocess_text(query_original)
 print(f"Query original: {query_original}")
 print(f"Query procesado: {query}")
 k = 5
 num_docs = len(df)
-top_k_results = retrieve_top_k(query, k, num_blocks, num_docs, idf_values)
+top_k_results = retrieve_top_k(query, k, block_num, num_docs, idf_values)
 print(top_k_results)
