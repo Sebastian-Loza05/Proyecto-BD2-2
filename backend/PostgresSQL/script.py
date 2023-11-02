000848aa862f8d761227bf6016573bb368fe9256
AaronCoorahua <ruben.coorahua@utec.edu.pe>
2023-11-02 00:53:30 -0500

    Actualizacion de metodos

---
+++ backend/PostgresSQL/script.py
@@ -0,0 +1,42 @@
+import psycopg2
+import time
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="BD2Proyecto2",
+    user="postgres",
+    port = 5432,
+    password="123"
+)
+
+cur = conn.cursor()
+
+def search(Q, k):
+    query = Q.split(" ")
+    query = '|'.join(query)
+    top_k = k
+
+    start = time.time()
+
+    cur.execute(f"""
+        SELECT track_artist, track_name, ts_rank_cd(full_text, query, 1) AS rank
+        FROM songslist, to_tsquery('english', '{query}') query
+        WHERE query @@ full_text
+        ORDER BY rank ASC
+        LIMIT {top_k};
+    """)
+
+    end = time.time()
+
+    result = cur.fetchall()
+    conn.commit()
+    conn.close()
+
+
+    for row in result:
+        print(row)
+
+    print("Tiempo de ejecucion :",
+          (end - start) * 10 ** 3, "ms")
+
+search("Justin Biber", 100)
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from app import app  
 import indice
+import database
 
 
 @app.route('/', methods=['GET'])
@@ -9,18 +10,24 @@ def hello():
 
 @app.route('/invert_index', methods=['POST'])
 def invert_index():
-    
+
     query_text = request.json.get('query_text')
     top_k = int(request.json.get('top_k'))
     query_processed = indice.preprocess_text(query_text)
     results = indice.retrieve_top_k(query_processed, top_k, indice.merged_index, indice.num_docs)
     results_list = results.to_dict(orient='records')
-    
     return jsonify(results_list)
 
-@app.route('/psql', methods=['GET'])
+@app.route('/psql', methods=['POST'])
 def psql():
-    return "Hello from psql"
+    data = request.get_json()
+    query = data["query_text"]
+    top_k = int(data["top_k"])
+
+    conn = database.connect() 
+    results = database.search(conn, query, top_k)  
+    conn.close() 
+    return jsonify(results)
 
 @app.route('/mongo', methods=['GET'])
 def mongo():
