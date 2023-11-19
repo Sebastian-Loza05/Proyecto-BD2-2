5da40e288375439dcb3bd9f4792e5ffbbd4d7fc2
Sebastian-Loza05 <sebastian.loza@utec.edu.pe>
2023-11-19 17:26:56 -0500

    rtree endpoint

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,11 @@
-from flask import request, jsonify
+from flask import request, jsonify, abort
 from app import app
 import indiceInvertido as invert
 import database
+import ffmpeg
+import os
+from indice_multidimensional.rtree_method import knn_search, get_vector
+import json
 
 
 @app.route('/', methods=['GET'])
@@ -31,4 +35,38 @@ def psql():
 @app.route('/mongo', methods=['GET'])
 def mongo():
     return "Hello from mongo"
+
+@app.route('/rtree', methods=['POST'])
+def search_rtree():
+    error_422 = False
+    output = "uploads/output.wav"
+    try:
+        audio = request.files['audio']
+
+        if not audio:
+            error_422 = True
+            abort(422)
+
+        data = request.form.get('json')
+        json_data = json.loads(data)
+        top_k = json_data['topK']
+
+        save_file = f'uploads/{audio.filename}'
+        audio.save(save_file)
+        ffmpeg.input(save_file).output(output).run()
+        os.remove(save_file)
+        vector = get_vector(output)
+        response = knn_search(vector, top_k)
+        os.remove(output)
+        return jsonify({
+            'success': True,
+            'results': response
+        })
+
+    except Exception as e:
+        print(e)
+        if error_422:
+            abort(422)
+        else:
+            abort(500)
 
